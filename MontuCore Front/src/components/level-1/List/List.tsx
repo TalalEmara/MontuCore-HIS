e4bb3a0ddc46dfb13625a9dfe9898dc1bd6bb335
TalalEmara <126427309+TalalEmara@users.noreply.github.com>
2025-12-06 01:56:57 +0200

    feat: List Component structure and responsive

---
+++ MontuCore Front/src/components/level-1/List/List.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import styles from './List.module.css';
+
+type CellProps = {
+  children: React.ReactNode;
+};
+
+function Cell({ children }: CellProps) {
+  return <div className={styles.cell}>{children}</div>;
+}
+
+type RowProps = {
+  cells: React.ReactNode[];
+};
+
+function Row({ cells }: RowProps) {
+  return (
+    <div className={styles.row}>
+      {cells.map((child, index) => (
+        <Cell key={index}>{child}</Cell>
+      ))}
+    </div>
+  );
+}
+
+type ListHeaderProps = {
+  headers: React.ReactNode[];
+};
+
+function ListHeader({ headers }: ListHeaderProps) {
+  return (
+    <div className={styles.listHeader}>
+      {headers.map((header, index) => (
+        <Cell key={index}>{header}</Cell>
+      ))}
+    </div>
+  );
+}
+
+type ElementCardProps = {
+  cells: React.ReactNode[];
+};
+
+function ElementCard({ cells }: ElementCardProps) {
+  // ElementCard is just one Row
+  return <Row cells={cells} />;
+}
+
+type ListProps = {
+  header: React.ReactNode[];
+  data: React.ReactNode[][];
+};
+
+function List({ header, data }: ListProps) {
+  return (
+    <>
+      <ListHeader headers={header} />
+      {data.map((rowCells, index) => (
+        <ElementCard key={index} cells={rowCells} />
+      ))}
+    </>
+  );
+}
+
+export default List;
