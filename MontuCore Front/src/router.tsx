7396355e3b11495c4928e027b498709c0db4b1ed
TalalEmara <126427309+TalalEmara@users.noreply.github.com>
2025-12-05 23:17:18 +0200

    feat: add router dev tools Front end

--- MontuCore Front/src/router.tsx
+++ MontuCore Front/src/router.tsx
@@ -1,6 +1,17 @@
-import { createRootRoute, createRoute, createRouter, Link, Outlet, RouterProvider } from "@tanstack/react-router";
+import {
+  createRootRoute,
+  createRoute,
+  createRouter,
+  Link,
+  Outlet,
+  RouterProvider
+} from "@tanstack/react-router";
+
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
 import PhysicianView from "./pages/PhysicianView";
 import CaseView from "./pages/CaseView";
+
 const rootRoute = createRootRoute({
   component: RootLayout,
 });
@@ -10,41 +21,44 @@ function RootLayout() {
   return (
     <div>
       <h1>My App</h1>
-      
-        {/* nav bar should be here  */}
-      <nav style={{ display: 'flex', gap: 8 }}>
+
+      {/* nav bar should be here */}
+      <nav style={{ display: "flex", gap: 8 }}>
         <Link to="/">Home</Link>
-        <Link to="/physician">physicain</Link>
+        <Link to="/physician">physician</Link>
         <Link to="/case">Case</Link>
       </nav>
 
-      {/* Child routes render here */}
       <Outlet />
     </div>
   );
 }
-// top-level user Views
-const physicianViewRoute = createRoute({
-    getParentRoute: () => rootRoute,
-    path: 'physician',
-    component: PhysicianView,
-})
-const CaseRoute = createRoute({
-    getParentRoute: () => rootRoute,
-    path: 'Case',
-    component: CaseView,
-})
 
+const physicianViewRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "physician",
+  component: PhysicianView,
+});
+
+const CaseRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "case",
+  component: CaseView,
+});
 
 const routeTree = rootRoute.addChildren([
- physicianViewRoute,
-    CaseRoute,
+  physicianViewRoute,
+  CaseRoute,
 ]);
 
-// 4) Create router instance
+// eslint-disable-next-line react-refresh/only-export-components
 export const router = createRouter({ routeTree });
 
-// 5) Export a top-level App component using the router
 export function AppRouter() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router}/>
+      <TanStackRouterDevtools router={router}/>
+    </>
+  );
 }
