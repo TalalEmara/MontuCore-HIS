94e6b506cdba56f28dd4e5e68019af21c2c1676a
TalalEmara <126427309+TalalEmara@users.noreply.github.com>
2025-12-07 02:42:58 +0200

    feat: treatments List along with list refactoring

--- MontuCore Front/src/components/level-0/List/List.tsx
+++ MontuCore Front/src/components/level-0/List/List.tsx
@@ -1,63 +1,45 @@
+// List.tsx
 import React from 'react';
 import styles from './List.module.css';
 
-type CellProps = {
-  children: React.ReactNode;
-};
-
-function Cell({ children }: CellProps) {
-  return <div className={styles.cell}>{children}</div>;
-}
-
-type RowProps = {
-  cells: React.ReactNode[];
-};
-
-function Row({ cells }: RowProps) {
-  return (
-    <div className={styles.row}>
-      {cells.map((child, index) => (
-        <Cell key={index}>{child}</Cell>
-      ))}
-    </div>
-  );
-}
-
-type ListHeaderProps = {
-  headers: React.ReactNode[];
-};
-
-function ListHeader({ headers }: ListHeaderProps) {
-  return (
-    <div className={styles.listHeader}>
-      {headers.map((header, index) => (
-        <Cell key={index}>{header}</Cell>
-      ))}
-    </div>
-  );
-}
-
-type ElementCardProps = {
-  cells: React.ReactNode[];
-};
-
-function ElementCard({ cells }: ElementCardProps) {
-  // ElementCard is just one Row
-  return <Row cells={cells} />;
-}
-
-// needs validation to be same number of columns in header and data
 type ListProps = {
-  header: React.ReactNode[];
+  header: string[];
   data: React.ReactNode[][];
+  gridTemplateColumns?: string;
+  listClassName?: string;
+  headerClassName?: string;
+  rowClassName?: string;
 };
 
-function List({ header, data }: ListProps) {
+function List({
+  header,
+  data,
+  gridTemplateColumns,
+  listClassName,
+  headerClassName,
+  rowClassName,
+}: ListProps) {
   return (
-    <div className={styles.list}>
-      <ListHeader headers={header} />
-      {data.map((rowCells, index) => (
-        <ElementCard key={index} cells={rowCells} />
+    <div className={`${styles.list} ${listClassName ?? ''}`}>
+      <div
+        className={`${styles.listHeader} ${headerClassName ?? ''}`}
+        style={{ gridTemplateColumns }}
+      >
+        {header.map((h) => (
+          <div key={h}>{h}</div>
+        ))}
+      </div>
+
+      {data.map((row, i) => (
+        <div
+          key={i}
+          className={`${styles.row} ${rowClassName ?? ''}`}
+          style={{ gridTemplateColumns }}
+        >
+          {row.map((cell, j) => (
+            <div key={j}>{cell}</div>
+          ))}
+        </div>
       ))}
     </div>
   );
