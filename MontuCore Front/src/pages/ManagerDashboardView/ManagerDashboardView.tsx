1537af29010af56344bd35964bee892ea6e53ce5
TalalEmara <126427309+TalalEmara@users.noreply.github.com>
2025-12-26 00:11:18 +0200

    integration: Semi integrated Manager Dashboard
    need optimization : too many connetions
    need whole user data

--- MontuCore Front/src/pages/ManagerDashboardView/ManagerDashboardView.tsx
+++ MontuCore Front/src/pages/ManagerDashboardView/ManagerDashboardView.tsx
@@ -4,42 +4,48 @@ import TopBar from "../../components/level-1/TopBar/TopBar";
 import Pagination from "../../components/level-0/Pagination/Pagination";
 import List from "../../components/level-0/List/List";
 import styles from "./ManagerDashboardView.module.css";
+import { useManagerDashboard } from "../../hooks/useManagerDashboard";
+import { da } from "zod/v4/locales";
 
-const athlete_data = [
-  { id: 1, athleteFullName: "Lionel Messi", athleteFieldPosition: "Forward", athleteJerseyNumber: 10, caseStatus: "ACTIVE", Severity: "MILD" },
-  { id: 2, athleteFullName: "Cristiano Ronaldo", athleteFieldPosition: "Striker", athleteJerseyNumber: 7, caseStatus: "RECOVERED", Severity: "MODERATE" },
-  { id: 3, athleteFullName: "Cristiano", athleteFieldPosition: "Midfielder", athleteJerseyNumber: 17, caseStatus: "ACTIVE", Severity: "SEVERE" },
-  { id: 4, athleteFullName: "Messi", athleteFieldPosition: "Defender", athleteJerseyNumber: 4, caseStatus: "ACTIVE", Severity: "CRITICAL" },
-  { id: 5, athleteFullName: "Mbappe", athleteFieldPosition: "Forward", athleteJerseyNumber: 7, caseStatus: "RECOVERED", Severity: "MILD" },
-  { id: 6, athleteFullName: "Mohamed salah", athleteFieldPosition: "Goalkeeper", athleteJerseyNumber: 1, caseStatus: "ACTIVE", Severity: "MODERATE" },
-];
+// const athlete_data = [
+//   { id: 1, athleteFullName: "Lionel Messi", athleteFieldPosition: "Forward", athleteJerseyNumber: 10, caseStatus: "ACTIVE", Severity: "MILD" },
+//   { id: 2, athleteFullName: "Cristiano Ronaldo", athleteFieldPosition: "Striker", athleteJerseyNumber: 7, caseStatus: "RECOVERED", Severity: "MODERATE" },
+//   { id: 3, athleteFullName: "Cristiano", athleteFieldPosition: "Midfielder", athleteJerseyNumber: 17, caseStatus: "ACTIVE", Severity: "SEVERE" },
+//   { id: 4, athleteFullName: "Messi", athleteFieldPosition: "Defender", athleteJerseyNumber: 4, caseStatus: "ACTIVE", Severity: "CRITICAL" },
+//   { id: 5, athleteFullName: "Mbappe", athleteFieldPosition: "Forward", athleteJerseyNumber: 7, caseStatus: "RECOVERED", Severity: "MILD" },
+//   { id: 6, athleteFullName: "Mohamed salah", athleteFieldPosition: "Goalkeeper", athleteJerseyNumber: 1, caseStatus: "ACTIVE", Severity: "MODERATE" },
+// ];
 
-const staff_data = [
-  { id: 1, clinicanName: "Dr. Ahmed", specialty: "Physiotherapy", activeCasesCount: 12 },
-  { id: 2, clinicanName: "Dr. Sarah", specialty: "Orthopedic", activeCasesCount: 8 },
-  { id: 3, clinicanName: "Dr. Mariam", specialty: "Sports Med", activeCasesCount: 15 },
-  { id: 4, clinicanName: "Dr. kareem", specialty: "Nutrition", activeCasesCount: 6 },
-  { id: 5, clinicanName: "Dr. Mohamed", specialty: "Physiotherapy", activeCasesCount: 10 },
-  { id: 6, clinicanName: "Dr. Mo", specialty: "Physiotherapy", activeCasesCount: 16 },
-];
+// const staff_data = [
+//   { id: 1, clinicanName: "Dr. Ahmed", specialty: "Physiotherapy", activeCasesCount: 12 },
+//   { id: 2, clinicanName: "Dr. Sarah", specialty: "Orthopedic", activeCasesCount: 8 },
+//   { id: 3, clinicanName: "Dr. Mariam", specialty: "Sports Med", activeCasesCount: 15 },
+//   { id: 4, clinicanName: "Dr. kareem", specialty: "Nutrition", activeCasesCount: 6 },
+//   { id: 5, clinicanName: "Dr. Mohamed", specialty: "Physiotherapy", activeCasesCount: 10 },
+//   { id: 6, clinicanName: "Dr. Mo", specialty: "Physiotherapy", activeCasesCount: 16 },
+// ];
 
 const ManagerDashboard: React.FC = () => {
   const [caseSearchQuery, setCaseSearchQuery] = useState("");
   const [staffSearchQuery, setStaffSearchQuery] = useState("");
   const [currentPage, setPage] = useState(1);
-
   const totalPages = 5;
-  const Total_medical_spent = 125450;
 
-  const totalCasesAnalysis = athlete_data.length;
-  const activeCasesAnalysis = athlete_data.filter(c => c.caseStatus === "ACTIVE").length;
-  const recoveredCasesAnalysis = athlete_data.filter(c => c.caseStatus === "RECOVERED").length;
+  const { data, isLoading, error } = useManagerDashboard(currentPage, 10);
+
+  const athlete_data = data?.dashboard.athletes || [];
+  const staff_data = data?.dashboard.clinicians || [];
+  const Total_medical_spent = data?.dashboard.financials.totalSpend || 0;
+
+  const totalCasesAnalysis = data?.dashboard.cases.total || 0;
+  const activeCasesAnalysis = data?.dashboard.cases.active || 0;
+  const recoveredCasesAnalysis = data?.dashboard.cases.recovered || 0;
 
   const severityAnalysisCounts = {
-    CRITICAL: athlete_data.filter(c => c.Severity === "CRITICAL").length,
-    SEVERE: athlete_data.filter(c => c.Severity === "SEVERE").length,
-    MODERATE: athlete_data.filter(c => c.Severity === "MODERATE").length,
-    MILD: athlete_data.filter(c => c.Severity === "MILD").length,
+    CRITICAL: data?.dashboard.cases.severity.critical || 0,
+    SEVERE: data?.dashboard.cases.severity.severe || 0,
+    MODERATE: data?.dashboard.cases.severity.moderate || 0,
+    MILD: data?.dashboard.cases.severity.mild || 0,
   };
 
   const pieAnalysisStyle = {
@@ -52,39 +58,39 @@ const ManagerDashboard: React.FC = () => {
   const mappedAthleteRows = useMemo(() => {
     return athlete_data
       .filter(c =>
-        c.athleteFullName.toLowerCase().includes(caseSearchQuery.toLowerCase()) ||
-        c.athleteFieldPosition.toLowerCase().includes(caseSearchQuery.toLowerCase()) ||
-        c.athleteJerseyNumber.toString().includes(caseSearchQuery) ||
-        c.caseStatus.toLowerCase().includes(caseSearchQuery.toLowerCase()) ||
-        c.Severity.toLowerCase().includes(caseSearchQuery.toLowerCase())
+        c.fullName.toLowerCase().includes(caseSearchQuery.toLowerCase())
+        // c.athleteFieldPosition.toLowerCase().includes(caseSearchQuery.toLowerCase()) ||
+        // c.athleteJerseyNumber.toString().includes(caseSearchQuery) ||
+        // c.caseStatus.toLowerCase().includes(caseSearchQuery.toLowerCase()) ||
+        // c.Severity.toLowerCase().includes(caseSearchQuery.toLowerCase())
       )
       .map(c => [
-        <span className={styles.athleteNameText}>{c.athleteFullName}</span>,
-        <span className={styles.athletePosition}>{c.athleteFieldPosition}</span>,
-        <span className={styles.athleteJerseyText}>#{c.athleteJerseyNumber}</span>,
-        <span className={c.caseStatus === "ACTIVE" ? styles.statusActiveText : styles.statusRecoveredText}>{c.caseStatus}</span>,
-        <span className={styles[`severity${c.Severity}Text`]}>{c.Severity}</span>,
+        <span className={styles.athleteNameText}>{c.fullName}</span>,
+        // <span className={styles.athletePosition}>{c.athleteFieldPosition}</span>,
+        // <span className={styles.athleteJerseyText}>#{c.athleteJerseyNumber}</span>,
+        // <span className={c.caseStatus === "ACTIVE" ? styles.statusActiveText : styles.statusRecoveredText}>{c.caseStatus}</span>,
+        // <span className={styles[`severity${c.Severity}Text`]}>{c.Severity}</span>,
       ]);
-  }, [caseSearchQuery]);
+  }, [athlete_data, caseSearchQuery]);
 
   const mappedStaffAnalysisRows = useMemo(() => {
     return staff_data
       .filter(s =>
-        s.clinicanName.toLowerCase().includes(staffSearchQuery.toLowerCase()) ||
-        s.specialty.toLowerCase().includes(staffSearchQuery.toLowerCase()) ||
-        s.activeCasesCount.toString().includes(staffSearchQuery)
+        s.fullName.toLowerCase().includes(staffSearchQuery.toLowerCase())
+        // s.specialty.toLowerCase().includes(staffSearchQuery.toLowerCase()) ||
+        // s.activeCasesCount.toString().includes(staffSearchQuery)
       )
       .map(s => [
-        <span className={styles.clinicanNameText}>{s.clinicanName}</span>,
-        <span className={styles.clinicanSpecialty}>{s.specialty}</span>,
-        <span className={styles.activeCasesCountBadge}>{s.activeCasesCount} Cases</span>,
+        <span className={styles.clinicanNameText}>{s.fullName}</span>,
+        // <span className={styles.clinicanSpecialty}>{s.specialty}</span>,
+        // <span className={styles.activeCasesCountBadge}>{s.activeCasesCount} Cases</span>,
       ]);
-  }, [staffSearchQuery]);
+  }, [staffSearchQuery, staff_data]);
 
   const appointmentStatusCounts = {
-    SCHEDULED: 4,
-    COMPLETED: 6,
-    CANCELLED: 2,
+    SCHEDULED: data?.dashboard.appointments.scheduled || 0,
+    COMPLETED: data?.dashboard.appointments.completed || 0,
+    CANCELLED: data?.dashboard.appointments.cancelled || 0,
   };
 
   const totalAppointments =
